6c1f71a718f1f9717e5e6948c5029ff410ca0f06
esby-space <77418085+esby-space@users.noreply.github.com>
2022-08-19 15:16:09 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# elephant cards!
+
+## idea
+a basic quizlet thingy with spatial repetition 
+
+## install
+
+```bash
+git clone --depth 1 https://github.com/esby-space/elephant.git
+cd elephant
+npm install
+npm run tauri dev
+```
+
+doesn't work at all :p
+
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-export {};
+import { readTextFile, writeTextFile, createDir, BaseDirectory } from "@tauri-apps/api/fs";
+
+const main = async () => {
+    const contents = await readTextFile("cards.json", {
+        dir: BaseDirectory.App,
+    }).catch(async () => {
+        await createDir("", { dir: BaseDirectory.App, recursive: true });
+        await writeTextFile("cards.json", "{}", { dir: BaseDirectory.App });
+        return "{}";
+    });
+
+    const cards = JSON.parse(contents);
+    console.log(cards, typeof(cards));
+};
+
+main();
+
